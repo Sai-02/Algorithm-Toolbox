6af6089f99addb2e6142808ab75dcdf1c51f8bd7
Sai Prashant Saxena <56087847+Sai-02@users.noreply.github.com>
2020-05-16 17:14:06 +0530

    Add files via upload

---
+++ week2_algorithmic_warmup/3_greatest_common_divisor/Solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+ class Solution
+ {
+//  
+ public static int gcd(int a, int b) 
+    { 
+        if (a == 0) 
+            return b; 
+          
+        return gcd(b%a, a); 
+    } 
+  public static void main(String args[]) {
+    Scanner scanner = new Scanner(System.in);
+    int a = scanner.nextInt();
+    int b = scanner.nextInt();
+
+    System.out.println(gcd(a, b));
+  }
+}
