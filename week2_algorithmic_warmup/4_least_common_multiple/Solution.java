f2f9febf15d678de9ff8577be92bc7b7f77c5d84
Sai Prashant Saxena <56087847+Sai-02@users.noreply.github.com>
2020-05-19 17:34:55 +0530

    Update Solution.java

--- week2_algorithmic_warmup/4_least_common_multiple/Solution.java
+++ week2_algorithmic_warmup/4_least_common_multiple/Solution.java
@@ -1,19 +1,20 @@
 import java.util.*;
 
- public class Solution {
-  private static long lcm_naive(int a, int b) {
-    for (long l = 1; l <= (long) a * b; l=l+a)
-      if (l % a == 0 && l % b == 0)
-        return l;
+public class Solution {
 
-    return (long) a * b;
+  //
+  public static long gcd(long a, long b) {
+    if (a == 0) return b;
+
+    return gcd(b % a, a);
   }
 
   public static void main(String args[]) {
     Scanner scanner = new Scanner(System.in);
-    int a = scanner.nextInt();
-    int b = scanner.nextInt();
-
-    System.out.println(lcm_naive(a, b));
+    long a = scanner.nextLong();
+    long b = scanner.nextLong();
+    long gcd = gcd(a, b);
+    long lcm = (a * b) / gcd;
+    System.out.println(lcm);
   }
 }
