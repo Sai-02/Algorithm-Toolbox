72e57bc59cda14a66ee10f373410b6c861459150
Sai Prashant Saxena <56087847+Sai-02@users.noreply.github.com>
2020-05-19 14:50:26 +0530

    Update Solution.java

--- week2_algorithmic_warmup/1_fibonacci_number/Solution.java
+++ week2_algorithmic_warmup/1_fibonacci_number/Solution.java
@@ -1,26 +1,29 @@
-/* package codechef; // don't place package name! */
-
-import java.util.*;
-import java.lang.*;
 import java.io.*;
+import java.lang.*;
+import java.util.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
-class Solution
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		 Scanner in = new Scanner(System.in);
-		 int i;
-               int n = in.nextInt();
-               int []fib=new int[n+1];
-               fib[0]=0;
-               fib[1]=1;
-               fib[2]=1;
-               for(i=3;i<n+1;i++){
-                   fib[i]=fib[i-1]+fib[i-2];
-               }
-              System.out.printf("%d",fib[n]);
+public class Solution {
 
-   
+  public static void main(String[] args) throws java.lang.Exception {
+    Scanner in = new Scanner(System.in);
+    int i;
+    int n = in.nextInt();
+    if (n >= 2) {
+      int[] fib = new int[n + 1];
+      fib[0] = 0;
+      fib[1] = 1;
+
+      for (i = 2; i < n + 1; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+      }
+      System.out.printf("%d", fib[n]);
+    } else {
+      if (n == 0) {
+        System.out.println("0");
+      }
+      if (n == 1) {
+        System.out.println("1");
+      }
+    }
   }
 }
