cd2e3b2f9e6f38ddef9589b4a94fbd87e5a5beb0
Sai Prashant Saxena <56087847+Sai-02@users.noreply.github.com>
2020-05-20 12:52:16 +0530

    Update Solution.java

--- week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/Solution.java
+++ week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/Solution.java
@@ -1,30 +1,27 @@
-import java.math.*;
 import java.util.*;
 
 public class Solution {
+    private static int getFibonacciLastDigitNaive(int n) {
+        if (n <= 1){
+            
+            return n;
+        }
 
-  private static int getFibonacciLastDigitNaive(int n) {
-    if (n <= 1) return n;
-
-    BigInteger previous = BigInteger.valueOf(0);
-    BigInteger current = BigInteger.valueOf(1);
-
-    for (int i = 2; i < n + 1; i++) {
-      BigInteger tmp_previous = BigInteger.valueOf(0);
-      tmp_previous = previous;
-      previous = current;
-      current = tmp_previous.add(current);
+        int [] fib=new int[n+1];
+        int i;
+        fib[0]=0;
+        fib[1]=1;
+        for(i=2;i<n+1;i++){
+            fib[i]=fib[i-1]%10 +fib[i-2]%10;
+        }
+        return fib[n]%10;
+        
     }
-
-    BigInteger Ten = BigInteger.valueOf(10);
-    int r = (current.mod(Ten)).intValue();
-    return r;
-  }
-
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    int n = scanner.nextInt();
-    int c = getFibonacciLastDigitNaive(n);
-    System.out.println(c);
-  }
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int c = getFibonacciLastDigitNaive(n);
+        System.out.println(c);
+    }
 }
